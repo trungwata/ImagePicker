843730215b4a91a17ef0416945dc6845abf422a5
Trung Doan <peter.le@watasoftware.com>
2025-01-13 22:11:05 +0700

    Fixed persiable permission exception BottomSheetGalerryProvider

--- imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/provider/BottomSheetGalleryProvider.kt
+++ imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/provider/BottomSheetGalleryProvider.kt
@@ -1,17 +1,10 @@
 package com.github.dhaval2404.imagepicker.provider
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.Manifest.permission.READ_MEDIA_IMAGES
-import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
-import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.github.dhaval2404.imagepicker.ImagePickerActivity
 import com.github.dhaval2404.imagepicker.R
-import com.github.dhaval2404.imagepicker.util.PermissionUtil
 
 /**
  * Select image from Storage
@@ -22,10 +15,6 @@ import com.github.dhaval2404.imagepicker.util.PermissionUtil
  */
 class BottomSheetGalleryProvider(activity: ImagePickerActivity) :
     BaseProvider(activity) {
-
-    companion object {
-        private const val GALLERY_INTENT_REQ_CODE = 4269
-    }
 
     // Mime types restrictions for gallery. By default all mime types are valid
     private val mimeTypes: Array<String>
@@ -63,68 +52,19 @@ class BottomSheetGalleryProvider(activity: ImagePickerActivity) :
      * Start Gallery Intent
      */
     private fun startGalleryIntent() {
-        if (PermissionUtil.isPhotoPermissionGranted(activity)){
-            appImagePicker.pickImage(maxImagesNum)
-        } else {
-            requestPhotoPermission()
-        }
+        appImagePicker.pickImage(maxImagesNum)
     }
 
-    // Register ActivityResult handler
-    private val requestPermissions = activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
-        startGalleryIntent()
-    }
-
-    private fun requestPhotoPermission(){
-        // Permission request logic
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            requestPermissions.launch(arrayOf(READ_MEDIA_IMAGES, READ_MEDIA_VISUAL_USER_SELECTED))
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            requestPermissions.launch(arrayOf(READ_MEDIA_IMAGES))
-        } else {
-            requestPermissions.launch(arrayOf(READ_EXTERNAL_STORAGE))
-        }
-    }
-
-//
-//    /**
-//     * Handle Gallery Intent Activity Result
-//     *
-//     * @param requestCode It must be {@link GalleryProvider#GALLERY_INTENT_REQ_CODE}
-//     * @param resultCode For success it should be {@link Activity#RESULT_OK}
-//     * @param data Result Intent
-//     */
-//    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if (requestCode == GalleryProvider.GALLERY_INTENT_REQ_CODE) {
-//            if (resultCode == Activity.RESULT_OK) {
-//                handleResult(data)
-//            } else {
-//                setResultCancel()
-//            }
-//        }
-//    }
 
     private fun handleResult(imageUris: List<Uri>?) {
         if (imageUris.isNullOrEmpty()) {
             setError(R.string.error_failed_pick_gallery_image)
             return
         }
-        imageUris.forEach {
-            takePersistableUriPermission(it)
-        }
         if (imageUris.size == 1) {
             activity.setImage(imageUris.first())
         } else {
             activity.setMultipleImages(imageUris)
         }
-    }
-
-
-    /**
-     * Take a persistable URI permission grant that has been offered. Once
-     * taken, the permission grant will be remembered across device reboots.
-     */
-    private fun takePersistableUriPermission(uri: Uri) {
-        contentResolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
     }
 }
